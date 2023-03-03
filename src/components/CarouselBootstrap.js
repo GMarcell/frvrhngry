fe85511fc3124acc51a248b832466ec33b03d5e1
Grand Marcell <grand1310marcell@gmail.com>
2023-03-03 17:44:23 +0700

    add navbar

--- src/components/CarouselBootstrap.js
+++ src/components/CarouselBootstrap.js
@@ -4,6 +4,7 @@ import model1 from '../asset/image/model/DSC_0712.jpg'
 import model2 from '../asset/image/model/DSC_0738.jpg'
 import model4 from '../asset/image/model/cream blkg - cowo 2.jpg'
 import model3 from '../asset/image/model/SF dpn - cowo.jpg'
+import Navbar from './Navbar';
 
 function CarouselBootstrap() {
   const [index, setIndex] = useState(0)
@@ -13,20 +14,23 @@ function CarouselBootstrap() {
   }
 
   return (
-    <Carousel activeIndex={index} onSelect={handleSelect} fade>
-      <Carousel.Item interval={1000}>
-        <div className='d-flex'>
-          <img className="d-block w-50" src={model2} alt="First slide" />
-          <img className="d-block w-50" src={model1} alt="First slide" />
-        </div>
-      </Carousel.Item>
-      <Carousel.Item interval={1000}>
-        <div className='d-flex'>
-          <img className="d-block w-50" src={model3} alt="Second slide" />
-          <img className="d-block w-50" src={model4} alt="Second slide" />
-        </div>
-      </Carousel.Item>
-    </Carousel>
+    <>
+      <Navbar/>
+      <Carousel activeIndex={index} onSelect={handleSelect} fade>
+        <Carousel.Item interval={1000}>
+          <div className='d-flex'>
+            <img className="d-block w-50" src={model2} alt="First slide" />
+            <img className="d-block w-50" src={model1} alt="First slide" />
+          </div>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <div className='d-flex'>
+            <img className="d-block w-50" src={model3} alt="Second slide" />
+            <img className="d-block w-50" src={model4} alt="Second slide" />
+          </div>
+        </Carousel.Item>
+      </Carousel>
+    </>
   )
 }
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import { AiOutlineInstagram, AiOutlineMail } from 'react-icons/ai';
 import { BsPhone } from 'react-icons/bs';
+import Navbar from './Navbar';
 
 function Contact() {
   return (
-    <div style={{height: '100vh', backgroundColor:'#ffdc22'}} className='d-flex justify-content-center align-items-center'>
-      <div>
-        <div className='py-2 d-flex align-items-center'>
-          <AiOutlineInstagram size={56} />
-          <h3 className='px-2'>frvrhngry.co</h3>
-        </div>
-        <div className='py-2 d-flex align-items-center'>
-          <AiOutlineMail size={56} />
-          <h3 className='px-2'>foreverhungryforever@gmail.com</h3>
-        </div>
-        <div className='py-2 d-flex align-items-center'>
-          <BsPhone size={56} />
-          <h3 className='px-2'>+62 813 1865 4828</h3>
+    <>
+      <Navbar/>
+      <div style={{height: '100vh', backgroundColor:'#ffdc22'}} className='d-flex justify-content-center align-items-center'>
+        <div>
+          <div className='py-2 d-flex align-items-center'>
+            <AiOutlineInstagram size={56} />
+            <h3 className='px-2'>frvrhngry.co</h3>
+          </div>
+          <div className='py-2 d-flex align-items-center'>
+            <AiOutlineMail size={56} />
+            <h3 className='px-2'>foreverhungryforever@gmail.com</h3>
+          </div>
+          <div className='py-2 d-flex align-items-center'>
+            <BsPhone size={56} />
+            <h3 className='px-2'>+62 813 1865 4828</h3>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
