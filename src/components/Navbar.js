1e6e37cc4cd1c2aaf85f55192944e7042d218296
Grand Marcell <grand1310marcell@gmail.com>
2023-03-04 00:02:33 +0700

    make menu bold

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,10 +16,10 @@ function NavbarBootstrap() {
         <Navbar.Toggle aria-controls="basic-navbar-nav d-flex justify-content-end" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto d-flex justify-content-end" style={{width: '100%'}}>
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/shop">Shop</Nav.Link>
-            <Nav.Link href="/about-us">About Us</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/" className='fw-bold'>Home</Nav.Link>
+            <Nav.Link href="/shop" className='fw-bold'>Shop</Nav.Link>
+            <Nav.Link href="/about-us" className='fw-bold'>About Us</Nav.Link>
+            <Nav.Link href="/contact" className='fw-bold'>Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
