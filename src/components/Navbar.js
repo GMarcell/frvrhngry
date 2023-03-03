62b5edb66f2c9e78581a801674ad6f931c10df48
Grand Marcell <grand1310marcell@gmail.com>
2023-03-03 18:13:25 +0700

    add about us path

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ function Navbar() {
             <li className="nav-item"><a className="nav-link" href="#portfolio">Portfolio</a></li>
             <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
             <li className="nav-item"><a className="nav-link" href="#team">Team</a></li>
-            <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
+            <li className="nav-item"><a className="nav-link" href="/contact">Contact</a></li>
           </ul>
         </div>
       </div>
